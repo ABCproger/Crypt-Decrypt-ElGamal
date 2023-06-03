ce34ce8c37aefdbb44edf08df75a70104f8b6aa0
ABCproger <vladmelhnik@gmail.com>
2023-06-03 22:41:27 +0300

    final version of course work

--- Program.cs
+++ Program.cs
@@ -3,43 +3,131 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Diagnostics;
 using System.Security.Cryptography.X509Certificates;
 
 namespace El_Gamal
 {
     internal class Program
     {
-            static void Main(string[] args)
+        static void Main(string[] args)
         {
-            int g, x, p, k;
+            int g, x, p;
             List<int> cryptedText = new List<int>();
-            string message;
             Random random = new Random();
-            Console.WriteLine("Hi, its a ElGamal cryptology program ");
-           
-            Console.WriteLine("Please, enter a number");
-            g = int.Parse(Console.ReadLine());
-            Console.WriteLine("Please, enter a pow of number");
-            x = int.Parse(Console.ReadLine());                  // must be rand 1<x<p!!!!!!!
-            Console.WriteLine("Please, enter a mod");
-            p = int.Parse(Console.ReadLine()); // має бути простим числом
-            Console.WriteLine("Please, enter a message");
-            message = Console.ReadLine();
-            
-            cryptedText = crypt(p,g,x,message);
-            Console.WriteLine("Crypted text");
+            Console.WriteLine("Hi, its a ElGamal cryptology program \n");
 
-            for(int i = 1; i < cryptedText.Count; i++)
+            int chooseOperationNumb = chooseOperation();
+            if (chooseOperationNumb == 1)
             {
-                Console.Write(cryptedText[i] + " ");
+                Console.Write("Please, enter a way to file which you want to crypt: ");
+                string pathToCrypt = Console.ReadLine();
+                FileInfo fileInf = new FileInfo("pathToCrypt");
+                string messageFromFile = File.ReadAllText(pathToCrypt); // Зчитування тексту з файлу
+                Console.WriteLine("Succesful! text has been read from file, your text: ");
+                Console.WriteLine(messageFromFile);
+                do
+                {
+                    p = random.Next(2000);
+                } while (IsPrimeNumber(p) != true);
+                g = random.Next(1, p);
+                x = random.Next(1, p);
+                cryptedText = crypt(p, g, x, messageFromFile);
+                Console.WriteLine("Crypted text");
+
+                for (int i = 0; i < cryptedText.Count; i++)
+                {
+                    Console.Write(cryptedText[i] + " ");
+                }
+                Console.WriteLine();
+                bool ind = true;
+                while (ind)
+                {
+                    Console.Write("Do you want decrypt this crypted text?[Y/N]: ");
+                    string answer = Console.ReadLine();
+
+                    if (answer == "Y" || answer == "y")
+                    {
+                        decrypt(cryptedText, p, x);
+                        ind = false;
+                    }
+                    else if (answer == "N" || answer == "n")
+                    {
+                        Console.WriteLine();
+                        ind = false;
+                    }
+                    else ind = true;
+                }
             }
-            Console.WriteLine();
-            decrypt(cryptedText,p,x);
+            else if (chooseOperationNumb == 2)
+            {
+
+                Console.Write("Please, enter a way to file which you want to decrypt: ");
+                string pathToDecrypt = Console.ReadLine();
+                Console.WriteLine(pathToDecrypt);
+                using (StreamReader sr = new StreamReader(pathToDecrypt))
+                {
+                    Console.WriteLine("Succesfull! cypted text has been read from file ");
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        string[] numberStrings = line.Split(' '); // розділити рядок за допомогою пропуску
+
+                        foreach (string numberString in numberStrings)
+                        {
+                            int number = int.Parse(numberString); // конвертувати окреме число в тип int
+                            cryptedText.Add(number); // додати число до списку
+                        }
+                    }
+
+                    Console.Write("Please, enter a secret key: ");
+                    int secretKeyX = int.Parse(Console.ReadLine());
+                    Console.Write("Please, enter a mod: ");
+                    int modOfNumb = int.Parse(Console.ReadLine());
+                    decrypt(cryptedText, modOfNumb, secretKeyX);
+                }
+            }
+        }
+        static int chooseOperation()
+        {
+            int chooseOperationNumb = 0;
+            bool ind = true;
+            while (ind)
+            {
+                Console.WriteLine("Please, choose a function what do you want to do: \n");
+                Console.WriteLine("1. Crypt your text file: \n2. Decrypt your text file:");
+                chooseOperationNumb = int.Parse(Console.ReadLine());
+                if (chooseOperationNumb < 1 || chooseOperationNumb > 2)
+                {
+                    Console.WriteLine("Error number of operation lets try now ");
+                    ind = true;
+                }
+                else ind = false;
+            }
+            return chooseOperationNumb;
+        }
+
+        public static bool IsPrimeNumber(int n)
+        {
+            var result = true;
+
+            if (n > 1)
+            {
+                for (var i = 2; i < n; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        result = false;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                result = false;
+            }
+
+            return result;
         }
 
         static int powerModp(int x, int y, int n) // x^y mod n піднесення основи а до степеня b за модулем N
@@ -72,11 +160,11 @@ namespace El_Gamal
             int y, a, b, stepY;
             Random random = new Random();
             List<int> cryptedText = new List<int>();
-           int k = random.Next() % (p - 2) + 1;            // k must be (1<k<p-1)
+            int k = random.Next() % (p - 2) + 1;            // k must be (1<k<p-1)
             Console.WriteLine("k: " + k);
             y = powerModp(g, x, p);
             Console.WriteLine("Open key(p,g,y): " + p + "  " + g + " " + y);
-            Console.WriteLine("Close key (x)" + x);
+            Console.WriteLine("Close key (x)" + x); 
             if (message.Length > 0)
             {
                 char[] temp = new char[message.Length - 1];
@@ -87,13 +175,7 @@ namespace El_Gamal
                 for (int i = 0; i <= message.Length - 1; i++)
                 {
                     int tempStr = Convert.ToInt32(temp[i]);
-                    
-                       // Console.Write(tempStr + " ");
-                    
-                    //Console.WriteLine("msg: " + tempStr);
                     b = multiplication(stepY, tempStr, p);     // b= y^k * msg mod p
-                    //Console.WriteLine("TEST "+b);                            // Друга частина шифротексту
-                    //Console.WriteLine("ENCRYPT TEST "+ a);                   // перша чатсина шифротексту
                     cryptedText.Add(b);
                 }
             }
@@ -102,20 +184,18 @@ namespace El_Gamal
 
         static void decrypt(List<int> cryptedText, int p, int x)
         {
-            int stepA, A,intMsg,B;
+            int stepA, A, intMsg, B;
             char msg;
             A = cryptedText[0];
-            //Console.WriteLine("A: " + A);
-            stepA = powerModp(A,p-1-x,p);
+            stepA = powerModp(A, p - 1 - x, p);
             Console.WriteLine("DECRYPTED TEXT");
-            for (int i = 1; i < cryptedText.Count; i++) 
+            for (int i = 1; i < cryptedText.Count; i++)
             {
                 B = cryptedText[i];
-                intMsg = multiplication(B,stepA,p);
+                intMsg = multiplication(B, stepA, p);
                 msg = Convert.ToChar(intMsg);
                 Console.Write(msg);
             }
-
         }
     }
 
